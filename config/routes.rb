a1c1af642ce6ea9d6e428b6c3dec0ea919988a5f
Brian Song <brian.cs.song@gmail.com>
2024-10-30 19:12:20 +0000

    Initial push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  get("/", { :controller => "zebra", :action => "rules" })
+
+  get("/square/new", { :controller => "zebra", :action => "square_new" })
   
+  get("/square_root/new", { :controller => "zebra", :action => "square_root" })
+
+  get("/payment/new", { :controller => "zebra", :action => "payment" })
+
+  get("/random/new", { :controller => "zebra", :action => "random_new" })
+
 end
