3428ad26eef3b5833ad9701817ec548d2e9a5940
Brian Song <brian.cs.song@gmail.com>
2024-10-30 19:22:53 +0000

    New change

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,9 +1,13 @@
 class ZebraController < ApplicationController
+  def rules
+    render({ :template => "calc_templates/rules"})
+  end
+  
   def square_new
     @the_num = params.fetch("number").to_f
 
     @the_result = @the_num ** 2
   
-    render({ :template => calc_templates/square_new})
+    render({ :template => "calc_templates/square_new"})
   end
 end
